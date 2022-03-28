5996eefef7e67d9ff4628bf03dbffd0e4ee84c92
Пользователь <@DESKTOP-4PNAQFH>
2022-03-28 12:40:53 +0300

    first commit

---
+++ REG_MARK_LIB/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace REG_MARK_LIB
+{
+    public class Class1
+    {
+        private char[] _acceptChars = new char[]
+        {
+            'A','B','E','K','M','H','O','P','C','T','Y','X'
+        };
+
+        public bool CheckMark(string mark)
+        {
+            if (mark.Length != 9)
+            {
+                return false;
+            }
+
+            if (!char.IsLetter(mark[0]) ||
+                mark.Substring(1, 3).Any(m => !char.IsDigit(m)) ||
+                mark.Substring(4, 2).Any(m => !char.IsLetter(m)) ||
+                mark.Substring(6, 3).Any(m => !char.IsDigit(m)))
+            {
+                return false;
+            }
+
+            if (mark.Any(m => !char.IsDigit(m) && !_acceptChars.Contains(m)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string GetNextMarkAfter(string mark)
+        {
+            if (!CheckMark(mark))
+            {
+                return mark;
+            }
+
+            int serie = int.Parse(mark.Substring(1, 3));
+            string regNum = mark.Substring(4, 2);
+            ++serie;
+
+            string firstLetter = mark[0].ToString();
+
+            if (serie == 1000)
+            {
+                firstLetter = _acceptChars[Array.IndexOf(_acceptChars, mark[0]) + 1].ToString();
+                serie = 000;
+            }
+            else if (regNum != "XX")
+            {
+                char newLettter = _acceptChars.ElementAtOrDefault(Array.IndexOf(_acceptChars, regNum[1]) + 1);
+                regNum = newLettter == default(char) ? _acceptChars[Array.IndexOf(_acceptChars, regNum[0]) + 1].ToString() + regNum[1].ToString() : regNum[0].ToString() + newLettter.ToString();
+                --serie;
+            }
+            else
+            {
+
+            }
+
+            mark = firstLetter + serie + regNum + mark.Remove(0, 6);
+            return mark;
+        }
+
+        public string GetNextMarkAfterInRange(string prevMark, string rangeStart, string rangeEnd)
+        {
+            return "";
+        }
+
+        public int GetCombinationsCountInRange(string mark1, string mark2)
+        {
+            return 0;
+        }
+    }
+}
---
+++ VIN_LIB/Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIN_LIB
+{
+    public class Class1
+    {
+        private char[] _acceptChars = new char[]
+        {
+            'A','B','C','D','E','F','G','H','J','K','L','M','N','P','R','S','T','U','V','W','X','Y','Z'
+        };
+
+        private string[] _lands = new string[]
+        {
+            "Африка", "Азия", "Европа", "Северная", "Америка", "Океания", " Южная Америка"
+        };
+
+        private Dictionary<string, string> _countries = new Dictionary<string, string>()
+        {
+            ["AA-AH"] = "ЮАР",
+            ["AJ-AN"] = "Кот-ид’Ивуар",
+            ["AP-A0"] = "не используется",
+            ["BA-BE"] = "Ангола",
+            ["BF-BK"] = "Кения",
+            ["BL-BR"] = "Танзания",
+            ["BS-B0"] = "не используется",
+            ["CA-CE"] = "Бенин",
+            ["CF-CK"] = "Мадагаскар",
+            ["CL-CR"] = "Тунис",
+            ["CS-C0"] = "не используется",
+            ["DA-DE"] = "Египет",
+            ["DF-DK"] = "Марокко",
+            ["DL-DR"] = "Замбия",
+            ["DS-D0"] = "не используется",
+            ["EA-EE"] = "Эфиопия",
+            ["EF-EK"] = "Мозамбик",
+            ["EL-E0"] = "не используется",
+            ["FA-FE"] = "Гана",
+            ["FF-FK"] = "Нигерия",
+            ["FL-F0"] = "не используется",
+            ["GA-G0"] = "не используется",
+            ["HA-H0"] = "не используется",
+            ["JA-JT"] = "Япония",
+            ["KA-KE"] = "Шри Ланка",
+            ["KF-KK"] = "Израиль",
+            ["KL-KR"] = "Южная Корея",
+            ["KS-K0"] = "Казахстан",
+            ["LA-L0"] = "Китай",
+            ["MA-ME"] = "Индия",
+            ["MF-MK"] = "Индонезия",
+            ["ML-MR"] = "Таиланд",
+            ["MS-M0"] = "не используется",
+            ["NF-NK"] = "Пакистан",
+            ["NL-NR"] = "Турция",
+            ["NT-N0"] = "не используется",
+            ["PA-PE"] = "Филиппины",
+            ["PF-PK"] = "Сингапур",
+            ["PL-PR"] = "Малайзия",
+            ["PS-P0"] = "не используется",
+            ["RA-RE"] = "ОАЭ",
+            ["RF-RK"] = "Тайвань",
+            ["RL-RR"] = "Вьетнам",
+            ["RS-R0"] = "Саудовская Аравия",
+            ["SA-SM"] = "Великобритания",
+            ["SN-ST"] = "Германия",
+            ["SU-SZ"] = "Польша",
+            ["S1-S4"] = "Латвия",
+            ["TA-TH"] = "Швейцария",
+            ["TJ-TP"] = "Чехия",
+            ["TR-TV"] = "Венгрия",
+            ["TW-T1"] = "Португалия",
+            ["UH-UM"] = "Дания",
+            ["UN-UT"] = "Ирландия",
+            ["UU-UZ"] = "Румыния",
+            ["U5-U7"] = "Словакия",
+            ["VA-VE"] = "Австрия",
+            ["VF-VR"] = "Франция",
+            ["VS-VW"] = "Испания",
+            ["VX-V2"] = "Сербия",
+            ["V3-V5"] = "Хорватия",
+            ["V6-V0"] = "Эстония",
+            ["W"] = "Германия",
+            ["XA-XE"] = "Болгария",
+            ["XF-XK"] = "Греция",
+            ["XL-XR"] = "Нидерланды",
+            ["XS-XW"] = "Россия(бывший СССР)",
+            ["XX-X2"] = "Люксембург",
+            ["X3-X0"] = "Россия",
+            ["YA-YE"] = "Бельгия",
+            ["YF-YK"] = "Финляндия",
+            ["YL-YR"] = "Мальта",
+            ["YS-YW"] = "Швеция",
+            ["YX-Y2"] = "Норвегия",
+            ["Y3-Y5"] = "Беларусь",
+            ["Y6-Y0"] = "Украина",
+            ["ZA-ZR"] = "Италия",
+            ["ZX-Z2"] = "Словения",
+            ["Z3-Z5"] = "Литва",
+        };
+
+        public bool CheckVIN(string vin)
+        {
+            if (vin.Any(v => !char.IsDigit(v) && !_acceptChars.Contains(v)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string GetVINCountry(string vin)
+        {
+            return "";
+        }
+
+        public int GetTransportYear(string vin)
+        {
+            if (vin.Length != 17)
+            {
+                return -1;
+            }
+
+            int result = 0;
+
+            char yearCode = vin[9];
+
+            if (char.IsDigit(yearCode))
+            {
+                result = 2001 - 49 + (int)yearCode;
+            }
+            else if (char.IsLetter(yearCode))
+            {
+                result = 1980 - 65 + (int)yearCode;
+            }
+
+            return result;
+        }
+    }
+}
